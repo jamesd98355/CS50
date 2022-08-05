ad12a37f82a884e1fbed1c69f83120269f0faf2b
jamesd98355 <james.donnelly246@hotmail.com>
2022-08-05 22:22:20 +0200

    finish readability task

--- readability/main.c
+++ readability/main.c
@@ -1,35 +1,78 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 
-int count_numbers(char text);
-int count_words(char text);
-int count_sentences(char text);
+int count_letters(char text[]);
+int count_words(char text[]);
+int count_sentences(char text[]);
 
 
-int main(void)
-{
-    char text[500];
+int main(void) {
+    char text[1000];
+    float letters;
+    float words;
+    float sentences;
 
     //take user input of text
     scanf("%[^\n]s", text);
 
     //count number of letters
-    printf("%i", count_numbers(*text));
+    letters = count_letters(text);
 
     //count number of  words seperated with a space
+    words = count_words(text);
 
+    //count number of sentences with ". ! ?"
+    sentences = count_sentences(text);
 
-    //count number of sentencces with ". ! ?"
+    // equation given to calculate readability of text
+    float L = letters/words * 100;
+    float S = sentences/words * 100;
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+    int rounded_index = (int)round(index);
+
+    if (rounded_index >= 16)
+    {
+        printf("Grade 16+");
+    }
+    else if (index < 1)
+    {
+        printf("Before Grade 1");
+    }
+    else
+    {
+        printf("Grade: %i", rounded_index);
+    }
 }
 
-int count_numbers(char text)
+int count_letters(char text[]) {
+    int count = 0;
+
+    for (int i = 0; i < strlen(text); i++) {
+        if (isalpha(text[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_words(char text[])
+{
+    int count = 1;
+    for (int i = 0; i < strlen(text); i++) {
+        if (text[i] == ' ') {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_sentences(char text[])
 {
     int count = 0;
-    for(int i = 0; i < strlen(&text); i++)
-    {
-        if (isalpha(text[i]))
-        {
+    for (int i = 0; i < strlen(text); i++) {
+        if (text[i] == '!' || text[i] == '?' || text[i] == '.') {
             count++;
         }
     }

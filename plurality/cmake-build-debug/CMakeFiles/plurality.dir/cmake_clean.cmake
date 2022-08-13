9f1e0eef16c4546480baad01106d96daffeb5263
jamesd98355 <james.donnelly246@hotmail.com>
2022-08-13 11:37:28 +0200

    initial plurality commit

---
+++ plurality/cmake-build-debug/CMakeFiles/plurality.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plurality.dir/main.c.o"
+  "plurality"
+  "plurality.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/plurality.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

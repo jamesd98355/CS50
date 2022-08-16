31ef850a23d2e26e4338c4079e47ca340983856f
Jane Owen <janeowen@Janes-iMac.local>
2022-08-16 11:34:59 +0200

    filter function start

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,26 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    //average of all 3 RGB values and make them the same
+
+    int bue, green, red, average;
+    for (int i = 0; i height i++)
+    {
+        for (int j = 0; i < width; i++)
+        {
+            blue  = image[i][j].rgbtBlue;
+            green = image[i][j].rgbtGreen;
+            red   = image[i][j].rgbtRed;
+
+            average = blue+green+red / 3;
+
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+
+        }
+    }
+
     return;
 }
 

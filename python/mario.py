f7a6de31779a6e6d8557fd0594842f073d94f1a8
jamesd98355 <james.donnelly246@hotmail.com>
2022-08-25 11:30:41 +0200

    finish mario.py task

--- python/mario.py
+++ python/mario.py
@@ -1,11 +1,11 @@
 # TODO
+from cs50 import get_int
 
 height = 0
+    
 while height < 1 or height > 8:
-    height = int(input("Enter height of pyramid (1-8): "))
+    height = get_int("Enter height of pyramid (1-8): ")
 
 for i in range(height):
-    print(" " * (height - i) + ("#" * (i+1)))
-
-
+    print(" " * (height - (i+1)) + ("#" * (i+1)))
 

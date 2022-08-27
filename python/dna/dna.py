faafeef4d7a22da8b017bf36ddd0616b0066a9e5
jamesd98355 <james.donnelly246@hotmail.com>
2022-08-27 16:38:28 +0200

    finished dna task

--- python/dna/dna.py
+++ python/dna/dna.py
@@ -1,22 +1,52 @@
 import csv
+from nis import match
 import sys
 
 
 def main():
 
     # TODO: Check for command-line usage
+    # filename databases sequences
+    if len(sys.argv) != 3:
+        sys.exit("Usage: python dna.py database sequence")
 
-    
 
     # TODO: Read database file into a variable
-    
+    dna_database = []
+    with open(sys.argv[1]) as file:
+        dna_reader = csv.DictReader(file)
+        dna_database = list(dna_reader)
+
     # TODO: Read DNA sequence file into a variable
+    dna_sequence = []
+    with open(sys.argv[2]) as file:
+        dna_sequence = file.read()
+    
+
+    
+
 
     # TODO: Find longest match of each STR in DNA sequence
+    matches = {} #create empty dictionary
+    for i in dna_database[0]: #where is name / AGATC ...
+        matches[i] = (longest_match(dna_sequence , i))
 
     # TODO: Check database for matching profiles
+    # go through database 1 line at a time, if a str matches add 1 to counter...
+    # if counter is the same as strs length then all strs match row therefore found a match.
+    # else, continue for all rows
+    suspect = 'No Match'
+    match_counter = 1
+    for i in range(len(dna_database)):
+        for j in matches:
+            if str(matches[j]) == dna_database[i][j]:
+                match_counter += 1
+        if match_counter == len(matches):
+            suspect = dna_database[i]['name']
+            break
+        match_counter = 1
 
-    return
+    print(suspect)
 
 
 def longest_match(sequence, subsequence):
